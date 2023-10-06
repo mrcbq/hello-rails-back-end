2b91bad665d1acaf2f44507a68bac833eeb7d54a
mrcbq <marcobaquero@gmail.com>
2023-10-06 08:37:23 -0500

    delete test folder, add .rubocop conf file, add greetings controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(message: 'Hello this is a first greeting')
+Greeting.create(message: 'Hello this is a second greeting')
+Greeting.create(message: 'Hello this is another greeting')
+Greeting.create(message: 'Hello this is again another greeting')
+Greeting.create(message: 'Hello this is the last greeting')
