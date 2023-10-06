f7d9d0211ece199237ff64cbb09c15157a292f5f
mrcbq <marcobaquero@gmail.com>
2023-10-06 08:49:10 -0500

    Add routes to greetings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'greetings/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :greetings, only: [:index]
 end
